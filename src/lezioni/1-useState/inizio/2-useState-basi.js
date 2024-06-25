a74b6270836e83e85eadcfac1dc59565b8dbe485
CappelliLeonardo <l.cappelli1995@gmail.com>
2024-06-25 15:05:50 +0200

    use-state/Basi

--- src/lezioni/1-useState/inizio/2-useState-basi.js
+++ src/lezioni/1-useState/inizio/2-useState-basi.js
@@ -1,17 +1,31 @@
-import React from "react";
-// Gli Hooks devono essere usasti con use davanti
+import React, { useState } from "react";
+// Gli Hooks devono essere usati con use davanti
 // componenti devono avere la prima lettere Maiuscola
 // Gli Hook devono essere invocati all'interno del corpo/funzione del componente
 // Gli Hook non possono essere utilizzati in maniera condizionale
 
 const UsoBase = () => {
-  //   const value = useState()[0];
-  //   const handler = useState()[1];
-  //   console.log(value, handler);
+  //destrutturazione della funzione log
+  //const {log} = console;
+  //log(useState("Hello World!!"));
 
+  // const value = useState()[0];
+  // const handler = useState()[1];
+  // console.log(value, handler);
+  const [titolo, setTitolo] = useState('Hello World!!');
+  const cambiaTitolo = () => {
+    if (titolo === "Hello World!!"){
+      setTitolo('Il Titolo è stato cambiato')
+    }else{
+      setTitolo('Hello World!!')
+    }
+  }
   return (
     <>
-      <h2> Basi useState</h2>
+      <h2> {titolo}</h2>
+      <button type= 'button' className = 'btn btn-info shadow my-5' onClick={cambiaTitolo}>
+        Change me 
+      </button>
     </>
   );
 };
