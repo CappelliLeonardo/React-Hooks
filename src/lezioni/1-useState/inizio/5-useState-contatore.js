738e6b9656e2fd7eaa62aacf19ed63a9a1ac1b03
CappelliLeonardo <l.cappelli1995@gmail.com>
2024-06-25 19:52:34 +0200

    use-state/Functional return

--- src/lezioni/1-useState/inizio/5-useState-contatore.js
+++ src/lezioni/1-useState/inizio/5-useState-contatore.js
@@ -3,23 +3,30 @@ import React, {useState} from "react";
 const CounterComponent = () => {
   const [contatore, setContatore] = useState(0);
   const reset = () => setContatore(0);
-  const aumenta = () => {
-    setContatore((oldValue)=>{
-      if (oldValue + 1 === 11){
-        return oldValue;
-      }else{
-        return oldValue +1;
-      }
-    }
-    )
-  }
+  // const aumenta = () => {
+  //   setContatore((oldValue)=>{
+  //     if (oldValue + 1 === 11){
+  //       return oldValue;
+  //     }else{
+  //       return oldValue +1;
+  //     }
+  //   }
+  //   )
+  // }
+  const increase = () => {
+    setTimeout(() => {
+      setContatore((oldValue) => oldValue + 1);
+    }, 1000);
+  };
+
+  //Functional return - returna in base al valore
   return (
 
     <div className="bg-white shadow rounded py-5 w-75 col-6 offset-3">
       <h3>{contatore}</h3>
       <div className="d-flex justify-content-between">
         <button type="button" className="button my-2" onClick={()=>setContatore(contatore-1)}> Diminuisci</button>
-        <button type="button" className="button my-2" onClick={aumenta}> Aumenta</button>
+        <button type="button" className="button my-2" onClick={increase}> Aumenta</button>
       </div>
       <button type="button" className=" button reset-button my-2" onClick={reset}> Resetta</button>
 
