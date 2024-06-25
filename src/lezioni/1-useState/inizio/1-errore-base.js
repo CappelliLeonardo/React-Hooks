db9b0c88ff2b9f2979fb2b3fa42ddb83445ec28c
CappelliLeonardo <l.cappelli1995@gmail.com>
2024-06-25 14:24:10 +0200

    use-state/ErroreBase

--- src/lezioni/1-useState/inizio/1-errore-base.js
+++ src/lezioni/1-useState/inizio/1-errore-base.js
@@ -2,7 +2,18 @@ import React from "react";
 //Far vedere che il componente non si riaggiorna ma la variabile si
 //Spiegare che cosa è un React Fragment e che è possibile abbreviarlo
 const ErroreBase = () => {
-  return <div>Error Components</div>;
+
+  let titolo = "Hello World";
+  const cambiaTitolo = () =>{
+    titolo = 'Nuovo titolo';
+    console.log(titolo);
+  }
+  return <React.Fragment>
+    <h2>{titolo}</h2>
+    <button type = 'button' className = 'btn btn-info my-5' onClick={cambiaTitolo}>
+      Change title
+    </button>
+  </React.Fragment>;
 };
 
 export default ErroreBase;
