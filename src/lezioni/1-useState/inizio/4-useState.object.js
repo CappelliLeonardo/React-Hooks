785714b54ca7c10374b8b7c7c0f69c81157a3dce
CappelliLeonardo <l.cappelli1995@gmail.com>
2024-06-25 17:12:42 +0200

    use-state/Object

--- src/lezioni/1-useState/inizio/4-useState.object.js
+++ src/lezioni/1-useState/inizio/4-useState.object.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const StateObject = () => {
-  return <div>Object Componente UseState</div>;
+  //creo un oggetto persona
+  const [person, setPerson] = useState({name:'Dario', age: 30, saluto:'Ciao!'});
+
+  //creo funzione per cambiare l'oggetto
+  const cambiaSaluto = () => {
+    setPerson({
+      ...person, age:25,saluto:"Ciao, sono il saluto cambiato"
+    })
+  }
+  return<>
+    <div className="item shadow">
+      <div className="text-left">
+        <h5>{person.name}</h5>
+        <h5>{person.age}</h5>
+        <h6>{person.saluto}</h6>
+      </div>
+      <button type="button" className="button" onClick={cambiaSaluto}>
+        Clicca qui per cambiare qualcosa
+      </button>
+    </div>
+  </>;
 };
 
 export default StateObject;
